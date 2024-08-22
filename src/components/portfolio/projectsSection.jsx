0259542cfb5769cb2a907158715d7d40e78192a1
StructuredTrading <samowen1992@hotmail.com>
2024-08-22 21:23:26 +1000

    feat: Created basic sections for portfolio page

---
+++ src/components/portfolio/projectsSection.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function ProjectsSection() {
+    return (
+        <div className="projects-section">
+            <div className="projects-content">
+                <h3>Project Listings</h3>
+                <div>
+                    <h4>project 1</h4>
+                    <h5>Overview</h5>
+                    <h5>Technolgies used</h5>
+                    <h5>Challenges & Solutions</h5>
+                    <h5>Outcomes</h5>
+                    <h5>Links</h5>
+                </div>
+                <div>
+                    <h4>project 2</h4>
+                    <h5>Overview</h5>
+                    <h5>Technolgies used</h5>
+                    <h5>Challenges & Solutions</h5>
+                    <h5>Outcomes</h5>
+                    <h5>Links</h5>
+                </div>
+                <div>
+                    <h4>project 3</h4>
+                    <h5>Overview</h5>
+                    <h5>Technolgies used</h5>
+                    <h5>Challenges & Solutions</h5>
+                    <h5>Outcomes</h5>
+                    <h5>Links</h5>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectsSection;            
---
+++ src/pages/portfolioPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProjectsSection from "../components/portfolio/projectsSection";
+
+const PortfolioPage = () => {
+    return (
+        <div className='portfoliopage'>
+            <h1>Portfolio Page</h1>
+            <ProjectsSection />            
+        </div>
+    );
+}
+
+export default PortfolioPage;
