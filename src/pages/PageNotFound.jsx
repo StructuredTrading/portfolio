f907f379f11507ae18ab1b6a524290ce3a0fdee1
StructuredTrading <samowen1992@hotmail.com>
2024-08-23 15:23:08 +1000

    feat: added automatic page redirect on page not found to previously visited page in app or homepage

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PageNotFound = () => {
+    const [redirectTimer, setRedirectTimer] = useState(10);
+    const navigate = useNavigate();
+
+    const handleRedirectButton = () => {
+        const referrer = document.referrer;
+        const isInternalPage = referrer.includes(window.location.origin);
+        if (isInternalPage) {
+            navigate(-1);
+        } else {
+            navigate('/');
+        }
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setRedirectTimer((previousInterval) => previousInterval - 1);
+        }, 1000);
+
+        
+
+        return () => {
+            clearInterval(timer);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (redirectTimer === 0) {
+            handleRedirectButton();
+        }
+    }, [redirectTimer]);
+
+
+
     return (
         <div>
             <h1>404 - Page Not Found.</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
+            <p>Redirecting you to the previous page in {redirectTimer} seconds.</p>
+            <button onClick={handleRedirectButton}>Redirect Now</button>
         </div>
     );
 }
