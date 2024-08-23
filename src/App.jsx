f343ba98b630d3a278e2d747d2721e3cbb668225
StructuredTrading <samowen1992@hotmail.com>
2024-08-23 14:23:25 +1000

    feat: added page not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import AboutPage from './pages/aboutPage';
-import ContactPage from './pages/contactPage';
-import HomePage from './pages/homePage';
-import PortfolioPage from './pages/portfolioPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import PortfolioPage from './pages/PortfolioPage';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path='/' element={<HomePage />} />
         <Route path='/about' element={<AboutPage />} />
         <Route path='/portfolio' element={<PortfolioPage />} />
         <Route path='/contact' element={<ContactPage /> } />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </Router>
   );
